9062d56f0b10d33f6dadffaaced953c5bec4325c
tray0244 <tray024400@gmail.com>
2026-01-07 14:31:14 +0900

    [Setting] : 백엔드 프로젝트 세팅

---
+++ src/test/java/com/trillion/server/TrillionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trillion.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrillionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
