3d01c7d32dfda08cbaadb3ac22e55fe8ca1dab65
tray0244 <tray024400@gmail.com>
2026-01-08 15:44:12 +0900

    [Feat] : UserRepository 생성

--- src/main/java/com/trillion/server/users/repository/UserRepository.java
+++ src/main/java/com/trillion/server/users/repository/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.trillion.server.users.domain.UserEntity;
+import com.trillion.server.users.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
