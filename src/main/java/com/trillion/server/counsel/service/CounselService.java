6170ff6a452cbf191b4a42848ff70571fb26f377
tray0244 <tray024400@gmail.com>
2026-01-26 14:45:58 +0900

    [feat] : 트랜잭션 세분화 및 비동기 처리

--- src/main/java/com/trillion/server/counsel/service/CounselService.java
+++ src/main/java/com/trillion/server/counsel/service/CounselService.java
@@ -6,15 +6,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.trillion.server.common.exception.ErrorMessages;
 import com.trillion.server.counsel.dto.CounselDto;
+import com.trillion.server.counsel.entity.CounselCategory;
 import com.trillion.server.counsel.entity.CounselEntity;
 import com.trillion.server.counsel.entity.CounselStatus;
 import com.trillion.server.counsel.repository.CounselRepository;
 import com.trillion.server.users.entity.UserEntity;
 import com.trillion.server.users.repository.UserRepository;
+import io.swagger.v3.core.util.Json;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -22,6 +26,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
@@ -34,12 +39,13 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class CounselService {
     private final CounselRepository counselRepository;
     private final UserRepository userRepository;
-    private final RestTemplate restTemplate = new RestTemplate();
+//    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final TransactionTemplate transactionTemplate;
 
     @Value("${ai.server.url}")
     private String aiServerUrl;
@@ -66,38 +72,36 @@ public class CounselService {
     }
 
         @Async
-        @Transactional
         public void processAiAnalysis(Long counselId, CounselDto.CounselCreateRequest request) throws JsonProcessingException{
             CounselEntity counsel = counselRepository.findById(counselId)
                     .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.COUNSEL_NOT_FOUND));
 
-        // 1. 데이터 준비 (기존과 동일)
-        Map<String, String> aiRequest = new HashMap<>();
-        aiRequest.put("chat", request.chat());
-        aiRequest.put("date", request.date());
+            String aiResponseJson = null;
+            try{
+                Map<String, String> aiRequest = new HashMap<>();
+                aiRequest.put("chat", request.chat());
+                aiRequest.put("date", request.date());
 
-        String jsonBody = objectMapper.writeValueAsString(aiRequest);
+                String jsonBody = objectMapper.writeValueAsString(aiRequest);
+                HttpHeaders headers = new HttpHeaders();
+                headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
 
-        // 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                log.info("AI 서버 ({})로 분석 요청 전송...", aiServerUrl);
+                aiResponseJson = restTemplate.postForObject(aiServerUrl, entity, String.class);
+                CounselCategory category = extractCategory(aiResponseJson);
 
-        HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
-        log.info("AI request: {} ", entity.toString());
+                updateStatusInTransaction(counselId, CounselStatus.COMPLETED, aiResponseJson, category);
+                log.info(aiResponseJson);
+                log.info("AI 분석 성공. DB 업데이트");
 
-        try{
-            log.info("AI 서버 ({})로 분석 요청 전송...", aiServerUrl);
-            String aiResponseJson = restTemplate.postForObject(aiServerUrl, entity, String.class);
-            log.info(aiResponseJson);
-            log.info("AI 분석 성공. DB 업데이트");
-            counsel.completeAnalysis(aiResponseJson);
-        }catch (Exception e){
-            log.error("AI 서버 통신 실패: {}", e.getMessage());
-            counsel.failAnalysis();
+            }catch (Exception e){
+                log.error("AI 서버 통신 실패: {}", e.getMessage());
+                updateStatusInTransaction(counselId, CounselStatus.FAILED, null, null);
         }
     }
 
-    @Transactional
+
     public CounselDto.QuestionResponse question(Long userId, Long counselId, String question){
         CounselEntity counsel = counselRepository.findById(counselId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.COUNSEL_NOT_FOUND));
@@ -106,6 +110,7 @@ public class CounselService {
             throw new AccessDeniedException(ErrorMessages.FORBIDDEN);
         }
 
+        String currentSummaryJson = counsel.getSummaryJson();
         String ai_answer = "";
 
         try{
@@ -124,20 +129,26 @@ public class CounselService {
             log.info("AI 수신 완료,{}", ai_answer);
 
 
-            if(!ai_answer.isEmpty()){
-                String cleanAnswer = ai_answer;
-
+            if(ai_answer != null && !ai_answer.isEmpty()){
                 if (ai_answer.startsWith("\"") && ai_answer.endsWith("\"")) {
-                    cleanAnswer = ai_answer.substring(1, ai_answer.length() - 1)
+                    ai_answer = ai_answer.substring(1, ai_answer.length() - 1)
                             .replace("\\\"", "\"")  // 이스케이프된 따옴표 복구
                             .replace("\\n", "\n");  // 줄바꿈 복구
                 }
-                ai_answer = cleanAnswer;
             }
             log.info("AI 답변 정제 완료: {}", ai_answer);
+            String finalAnswer = ai_answer;
 
-
-            updateSummaryJson(counsel, question, ai_answer);
+            transactionTemplate.execute(status -> {
+                CounselEntity targetCounsel = counselRepository.findById(counselId)
+                        .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.COUNSEL_NOT_FOUND));
+                try{
+                    updateSummaryJson(targetCounsel, question, finalAnswer);
+                }catch (JsonProcessingException e){
+                    throw new RuntimeException("JSON 파싱 에러", e);
+                }
+                return null;
+            });
         }catch (Exception e){
             log.error("실패: {}", e.getMessage());
             throw new RuntimeException(ErrorMessages.COUNSEL_QUESTION_FAIL);
@@ -149,6 +160,7 @@ public class CounselService {
                 .build();
     }
 
+    @Transactional(readOnly = true)
     public List<CounselDto.CounselListResponse> getCounselList(Long userId){
         List<CounselEntity> counsels = counselRepository.findAllByUserIdOrderByCounselDateDesc(userId);
 
@@ -157,6 +169,7 @@ public class CounselService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public CounselDto.CounselDetailResponse getCounselDetail(Long userId, Long counselId) {
         CounselEntity counsel = counselRepository.findById(counselId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.COUNSEL_NOT_FOUND));
@@ -169,9 +182,7 @@ public class CounselService {
 
     private void updateSummaryJson(CounselEntity counsel, String question, String answer) throws JsonProcessingException{
         JsonNode rootNode = objectMapper.readTree(counsel.getSummaryJson());
-
         JsonNode resultNode = rootNode.path("data");
-
         JsonNode summaryRaw = resultNode.path("summary");
 
         if(summaryRaw.isMissingNode() || !summaryRaw.isObject()){
@@ -204,5 +215,30 @@ public class CounselService {
         String updatedJson = objectMapper.writeValueAsString(rootNode);
         counsel.completeAnalysis(updatedJson);
         log.info("CounselEntity 요약 JSON 업데이트 완료");
+    }
+
+    private void updateStatusInTransaction(Long counselId, CounselStatus status, String json, CounselCategory category){
+        transactionTemplate.execute(action -> {
+            CounselEntity counsel = counselRepository.findById(counselId)
+                    .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.COUNSEL_NOT_FOUND));
+
+            if(status == CounselStatus.COMPLETED) counsel.completeAnalysis(json, category);
+            else counsel.failAnalysis();
+
+            counselRepository.save(counsel);
+            counselRepository.flush();
+            return null;
+        });
+    }
+
+    private CounselCategory extractCategory(String jsonString) throws JsonProcessingException {
+        JsonNode root = objectMapper.readTree(jsonString);
+        String category = root.path("data").path("category").asText();
+
+        if(category == null || category.isBlank()){
+            throw new IllegalArgumentException(ErrorMessages.CATRGORY_NOT_FOUND);
+        }
+
+        return CounselCategory.from(category);
     }
 }
